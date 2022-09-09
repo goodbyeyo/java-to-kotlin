407e58c3068471f830d37e3a1c6661d963a5f75a
Sangwook Hwang <yoonseol1983@gmail.com>
2022-09-10 00:35:04 +0900

    beware of platform types when using java code in kotlin

--- src/main/java/com/lannstark/lec02/Person.java
+++ src/main/java/com/lannstark/lec02/Person.java
@@ -1,18 +1,30 @@
 package com.lannstark.lec02;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@Getter
+@RequiredArgsConstructor
 public class Person {
 
+  @NotNull
   private final String name;
 
-  public Person(String name) {
-    this.name = name;
-  }
+  @Nullable
+  private final String email;
 
-  public String getName() {
-    return name;
-  }
+//  public Person(String name) {
+//    this.name = name;
+//  }
+//  @NotNull
+//  public String getName() {
+//    return name;
+//  }
+
+
 
 }
 
