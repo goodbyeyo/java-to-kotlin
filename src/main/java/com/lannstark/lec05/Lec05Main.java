90d6622b6cd35ee5730349339e0fe184d7515970
Sangwook Hwang <yoonseol1983@gmail.com>
2022-10-16 20:13:18 +0900

    kotlin when

--- src/main/java/com/lannstark/lec05/Lec05Main.java
+++ src/main/java/com/lannstark/lec05/Lec05Main.java
@@ -9,7 +9,56 @@ public class Lec05Main {
 
   private void validateScoreIsNotNegative(int score) {
     if (score < 0) {
-      throw new IllegalArgumentException(String.format("%는 0보다 작을 수 없습니다"));
+      throw new IllegalArgumentException(String.format("%s는 0보다 작을 수 없습니다"));
+    }
+  }
+
+  private void validateScore(int score){
+    if (0 <= score && score <= 100) {
+      throw new IllegalArgumentException(String.format("%s는 0부터 100 사이여야 합니다"));
+    }
+  }
+
+  public String getPassOrFail(int score) {
+    return score >= 50 ? "P" : "F";
+  }
+
+  // 2. if-else
+  private String getGrade(int score) {
+    if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else {
+      return "D";
+    }
+  }
+
+  // 3. switch
+  private String getGradeWithSwitch(int score) {
+    return switch (score / 10) {
+      case 9 -> "A";
+      case 8 -> "B";
+      case 7 -> "C";
+      default -> "D";
+    };
+  }
+
+  private boolean startsWithA(Object obj{
+    if(obj instanceof String) {
+      return ((String) obj).startsWith("A");
+    }else {
+      return false;
+    }
+  }
+
+  private void judgeNumber(int number) {
+    if(number == 1 || number == 0 || number == -1 ){
+      System.out.println("number = " + number);
+    }else{
+      System.out.println("1, 0, -1 이 아닙니다");
     }
   }
 
@@ -24,7 +73,7 @@ public class Lec05Main {
       return;
     }
 
-    System.out.println("주어지는 숫자는 홀수입니다");
+    System.out.println("주어진 숫자는 홀수입니다");
   }
 
 }
